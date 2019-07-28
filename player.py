7f2c7a64578a1836a9c333476f686c89d1d15b9c
ma_1750 <mc.ma1750@gmail.com>
2019-07-29 05:24:13 +0900

    change loop implementation

--- player.py
+++ player.py
@@ -8,34 +8,25 @@ from opus_loader import load_opus_libs
 lock = asyncio.Lock()
 
 def play(player_queue, vc, loop):
-    asyncio.set_event_loop(loop)
-    loop.create_task(_play(player_queue, vc))
-
-
-async def _play(player_queue, vc):
-
-    async def _wait_packet():
-        '''
-        音声パケットがplayer_queueにたまるまで10msスリープ
-        '''
-        await asyncio.sleep(0.01)
-
     count = 0
     output: bytearray = []
-    while True:
-        await lock.acquire()
-        if player_queue.empty():
-            lock.release()
-            await asyncio.wait_for(_wait_packet(), timeout=0.5)
-            continue
-        else:
-            data = player_queue.get_nowait()
-            lock.release()
-            count += 1
-            output.append(data)
+    vc.loop.create_task(_play(player_queue, vc, count, output))
 
-        if count == 10:
-            for s in output:
-                vc.send_audio_packet(s, encode=False)
-            count = 0
-            output.clear()
+
+async def _play(player_queue, vc, count, output):
+    await lock.acquire()
+    if player_queue.empty():
+        lock.release()
+        await asyncio.sleep(0.01)
+    else:
+        data = player_queue.get_nowait()
+        lock.release()
+        count += 1
+        output.append(data)
+
+    if count == 10:
+        for s in output:
+            vc.send_audio_packet(s, encode=False)
+        count = 0
+        output.clear()
+    vc.loop.create_task(_play(player_queue, vc, count, output))
